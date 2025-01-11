10116c7906549a5b84011cf8424b78f32da9b2b0
HuBaSiaK <dabaydakov@mail.ru>
2025-01-11 20:02:43 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+# Объявление переменных
+username = 'Байдаков Денис'
+title = 'Captains log. Stardate 57436.2'
+content = 'First entry'
+status = 'Активна'
+created_date = "11-01-2025"
+issue_date = "11-01-2025"
+# Вывод переменных
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
