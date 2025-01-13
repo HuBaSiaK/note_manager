c9d7de5e3cb14486928139ade59d769a4c7ffde7
HuBaSiaK <dabaydakov@mail.ru>
2025-01-13 18:58:22 +0700

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,3 +1,6 @@
 issue_date = input('Введите дату') #можно задать и конкретную дату, но как понимаю это будет функция
 temp_issue_date = issue_date[0:5]
 print(temp_issue_date)
+created_date = input('Введите дату') #можно задать и конкретную дату, но как понимаю это будет функция
+temp_created_date = created_date[0:5]
+print(temp_created_date)
